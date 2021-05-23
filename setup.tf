79c7857f4c6b5ca76e2ac60c240b90d223d957c8
AnantV-dev <54234852+AnantV-dev@users.noreply.github.com>
2021-05-23 11:39:20 +0530

    vpc and subnet added

--- setup.tf
+++ setup.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+  region     = "us-east-2"
+}
 
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_subnet" "tf-pub-subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+  tags = {
+    Name = "tf-pub-subnet"
+  }
+
+
+resource "aws_subnet" "tf-pri-subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+  tags = {
+    Name = "tf-pri-subnet"
+  }
