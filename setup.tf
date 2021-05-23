a8c7728af9b4eb19ec00f5f0808f362765709ffc
AnantV-dev <54234852+AnantV-dev@users.noreply.github.com>
2021-05-23 11:44:32 +0530

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -12,16 +12,15 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "tf-pub-subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-}
   tags = {
     Name = "tf-pub-subnet"
   }
-
+}
 
 resource "aws_subnet" "tf-pri-subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-}
   tags = {
     Name = "tf-pri-subnet"
   }
+}
