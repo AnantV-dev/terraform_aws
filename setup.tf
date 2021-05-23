1638835712472fe8be7a8862d273b8797eadfcd3
AnantV-dev <54234852+AnantV-dev@users.noreply.github.com>
2021-05-23 12:02:10 +0530

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -2,12 +2,16 @@ provider "aws" {
   region     = "us-east-2"
 }
 
+/*==== VPC ======*/
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "terraform"
   }
 }
+
+/*==== Subnets ======*/
 
 resource "aws_subnet" "tf-pub-subnet" {
   vpc_id     = aws_vpc.vpc.id
@@ -19,8 +23,33 @@ resource "aws_subnet" "tf-pub-subnet" {
 
 resource "aws_subnet" "tf-pri-subnet" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   tags = {
     Name = "tf-pri-subnet"
+  }
+}
+/*==== Security Groups ======*/
+resource "aws_security_group" "Public" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
