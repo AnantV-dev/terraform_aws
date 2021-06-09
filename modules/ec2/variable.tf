fd70d90342df466c985564da58bf459b3e6ae85d
AnantV-dev <54234852+AnantV-dev@users.noreply.github.com>
2021-06-09 21:21:19 +0530

    variable.tf created
    We need this file for variable declaration. Means all the components required we need to keep in this file. We can change the values according to the environment creation.

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "aws region"
+  default     = "us-east-2"
+}
+
+variable "ami" {
+  description = "This is the ami id for ec2 instance"
+  default     = "ami-077e31c4939f6a2f3"
+}
+
+variable "instance_type" {
+  description = "This is insyance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "This is the key used for ec2"
+  default     = "ec2key"
+}
+
+variable "subnet_id" {
+  description = "This is subnet"
+  default     = "subnet-0076cb001eb5a9f6b"
+}
+
+variable "sg_ids" {
+  description = "These are the sceurity groups for ec2"
+  type        = list(string)
+  default     = ["sg-0cef13f6258380bd4"]
+}
+
+variable "ec2_name" {
+  description = "This is the name for ec2"
+  default     = ""
+}
